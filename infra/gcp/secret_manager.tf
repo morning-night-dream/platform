cdd44269aae5b5def7f6f571a10911e1c4c12300
takokun778 <takokun.778@gmail.com>
2022-11-06 00:26:10 +0900

    fix(#90) prefixを指定できるように修正

--- infra/gcp/secret_manager.tf
+++ infra/gcp/secret_manager.tf
@@ -1,8 +1,8 @@
-resource "google_secret_manager_secret" "secret_mndp_core_api_key" {
-  secret_id = "mndp-core-api-key"
+resource "google_secret_manager_secret" "secret_core_api_key" {
+  secret_id = "${var.project_prefix}-core-api-key"
 
   labels = {
-    label = "mndp"
+    label = var.project_prefix
   }
 
   replication {
@@ -10,17 +10,17 @@ resource "google_secret_manager_secret" "secret_mndp_core_api_key" {
   }
 }
 
-resource "google_secret_manager_secret_version" "secret_mndp_core_api_key_version" {
-  secret = google_secret_manager_secret.secret_mndp_core_api_key.id
+resource "google_secret_manager_secret_version" "secret_core_api_key_version" {
+  secret = google_secret_manager_secret.secret_core_api_key.id
 
   secret_data = var.secret_core_api_key
 }
 
-resource "google_secret_manager_secret" "secret_mndp_core_database_url" {
-  secret_id = "mndp-core-database-url"
+resource "google_secret_manager_secret" "secret_core_database_url" {
+  secret_id = "${var.project_prefix}-core-database-url"
 
   labels = {
-    label = "mndp"
+    label = var.project_prefix
   }
 
   replication {
@@ -28,8 +28,8 @@ resource "google_secret_manager_secret" "secret_mndp_core_database_url" {
   }
 }
 
-resource "google_secret_manager_secret_version" "secret_mndp_core_database_url_version" {
-  secret = google_secret_manager_secret.secret_mndp_core_database_url.id
+resource "google_secret_manager_secret_version" "secret_core_database_url_version" {
+  secret = google_secret_manager_secret.secret_core_database_url.id
 
   secret_data = var.secret_core_database_url
 }
