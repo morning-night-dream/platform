fa7af4eb408de1bbe44d7ffc14267802d06aa813
takokun778 <takokun.778@gmail.com>
2022-11-06 16:43:56 +0900

    chore(#90) cloudbuildの設定を修正

--- infra/gcp/cloud_build.tf
+++ infra/gcp/cloud_build.tf
@@ -20,12 +20,15 @@ resource "google_cloudbuild_trigger" "core_trigger" {
     _API_KEY                  = google_secret_manager_secret_version.secret_core_api_key_version.name
     _DATABASE_URL             = google_secret_manager_secret_version.secret_core_database_url_version.name
     _REVISION_SERVICE_ACCOUNT = google_service_account.cloud_run_invoker.email
-    _SERVICE_NAME             = "core"
+    _SERVICE_NAME             = "${var.project_prefix}-core-cloud-run"
   }
 
   filename = var.core_cloud_build_file_path
 
-  service_account = google_service_account.cloud_build_invoker.email
+  service_account = google_service_account.cloud_build_invoker.id
 
-  depends_on = [google_service_account.cloud_build_invoker, google_service_account.cloud_run_invoker]
+  depends_on = [
+    google_service_account.cloud_build_invoker,
+    google_service_account.cloud_run_invoker
+  ]
 }
