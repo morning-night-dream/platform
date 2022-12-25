eb457f449f6e0c3ce53d910c9338c8c28b795162
takokun <takokun.778@gmail.com>
2022-12-26 00:01:37 +0900

    Revert "fix: migration"
    This reverts commit 2bb8accba05029e8abfe12757897ac9d3688b040.

--- backend/app/core/database/database.go
+++ backend/app/core/database/database.go
@@ -1,6 +1,9 @@
 package database
 
 import (
+	"context"
+	"log"
+
 	// postgres driver.
 	_ "github.com/lib/pq"
 	"github.com/morning-night-dream/platform/pkg/ent"
@@ -8,6 +11,11 @@ import (
 
 func NewClient(dsn string) *ent.Client {
 	client, err := ent.Open("postgres", dsn)
+
+	if err := client.Schema.Create(context.Background()); err != nil {
+		log.Fatalf("Failed create schema: %v", err)
+	}
+
 	if err != nil {
 		panic(err)
 	}
