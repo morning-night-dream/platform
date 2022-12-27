f6adbf396e73d603ceb4376adcde7cdc5d38b5b4
takokun <takokun.778@gmail.com>
2022-12-27 23:52:00 +0900

    test: e2e

---
+++ backend/e2e/article/share_test.go
@@ -0,0 +1,54 @@
+package article_test
+
+import (
+	"context"
+	"net/http"
+	"reflect"
+	"testing"
+
+	"github.com/bufbuild/connect-go"
+	"github.com/morning-night-dream/platform/e2e/article"
+	"github.com/morning-night-dream/platform/e2e/helper"
+	articlev1 "github.com/morning-night-dream/platform/pkg/proto/article/v1"
+)
+
+func TestE2EArticleShare(t *testing.T) {
+	t.Parallel()
+
+	url := "http://localhost:8081"
+
+	t.Run("記事が共有できる", func(t *testing.T) {
+		t.Parallel()
+
+		hc := &http.Client{
+			Transport: helper.NewAPIKeyTransport(t, "local"),
+		}
+
+		client := article.New(t, hc, url)
+
+		req := &articlev1.ShareRequest{
+			Url:         "http://www.example.com",
+			Title:       "title",
+			Description: "description",
+			Thumbnail:   "http://www.example.com/thumbnail.jpg",
+		}
+
+		res, err := client.Article.Share(context.Background(), connect.NewRequest(req))
+		if err != nil {
+			t.Fatalf("faile to article share: %s", err)
+		}
+
+		if !reflect.DeepEqual(res.Msg.Article.Url, req.Url) {
+			t.Errorf("Url = %v, want %v", res.Msg.Article.Url, req.Url)
+		}
+		if !reflect.DeepEqual(res.Msg.Article.Title, req.Title) {
+			t.Errorf("Title = %v, want %v", res.Msg.Article.Title, req.Title)
+		}
+		if !reflect.DeepEqual(res.Msg.Article.Description, req.Description) {
+			t.Errorf("Description = %v, want %v", res.Msg.Article.Description, req.Description)
+		}
+		if !reflect.DeepEqual(res.Msg.Article.Thumbnail, req.Thumbnail) {
+			t.Errorf("Thumbnail = %v, want %v", res.Msg.Article.Thumbnail, req.Thumbnail)
+		}
+	})
+}
---
+++ backend/e2e/health/check_test.go
@@ -0,0 +1,30 @@
+package health_test
+
+import (
+	"context"
+	"net/http"
+	"testing"
+
+	"github.com/bufbuild/connect-go"
+	"github.com/morning-night-dream/platform/e2e/health"
+	healthv1 "github.com/morning-night-dream/platform/pkg/proto/health/v1"
+)
+
+func TestE2EHealthCheck(t *testing.T) {
+	t.Parallel()
+
+	url := "http://localhost:8081"
+
+	t.Run("ヘルスチェックが成功する", func(t *testing.T) {
+		t.Parallel()
+
+		client := health.New(t, http.DefaultClient, url)
+
+		req := &healthv1.CheckRequest{}
+
+		_, err := client.Health.Check(context.Background(), connect.NewRequest(req))
+		if err != nil {
+			t.Errorf("faile to health check: %s", err)
+		}
+	})
+}
