f9bf3af4639f3952e939fe8d17a5889719901e75
takokun <takokun.778@gmail.com>
2023-01-15 01:41:40 +0900

    fix: cache

--- backend/internal/cache/client.go
+++ backend/internal/cache/client.go
@@ -4,18 +4,26 @@ import (
 	"context"
 	"errors"
 	"sync"
+	"time"
 
 	"github.com/morning-night-dream/platform/internal/model"
 )
 
 type Client struct {
 	lock  sync.Mutex
-	cache map[string]model.Auth
+	cache map[string]Cache
+}
+
+const ttl = 60 * time.Minute
+
+type Cache struct {
+	model.Auth
+	CreatedAt time.Time
 }
 
 func NewClient() *Client {
 	return &Client{
-		cache: make(map[string]model.Auth),
+		cache: make(map[string]Cache),
 	}
 }
 
@@ -23,8 +31,8 @@ func (c *Client) Get(ctx context.Context, key string) (model.Auth, error) {
 	c.lock.Lock()
 	defer c.lock.Unlock()
 
-	if val, ok := c.cache[key]; ok {
-		return val, nil
+	if val, ok := c.cache[key]; ok && val.CreatedAt.Before(time.Now().Add(ttl)) {
+		return val.Auth, nil
 	}
 
 	return model.Auth{}, errors.New("mis cache")
@@ -34,7 +42,19 @@ func (c *Client) Set(ctx context.Context, key string, val model.Auth) error {
 	c.lock.Lock()
 	defer c.lock.Unlock()
 
-	c.cache[key] = val
+	c.cache[key] = Cache{
+		Auth:      val,
+		CreatedAt: time.Now(),
+	}
+
+	return nil
+}
+
+func (c *Client) Delete(ctx context.Context, key string) error {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+
+	delete(c.cache, key)
 
 	return nil
 }
